c4b0de3f2dc769bb703edf73a98f4a8b1ad9b2aa
Nikalya_1398 <v.ninmathirasa@gmail.com>
2024-06-18 15:45:08 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'form';
+  dataobject:any={};
+  getdata(data:NgForm){
+    this.dataobject=data;
+  }
+  
+
+  show=false;
+
+  click(){
+    this.show=!this.show;  // this.show 1st false then click true then false then true
+  }
 }
